199bd4dd0a21bcaa79c58cdef9af6387f0fe78f9
Dmitry Ermashev <dima@bmstr.ru>
2025-10-31 13:17:57 +0300

    work in progress

--- cmd/vk2tg/migrations/0002_vk_post_updates.sql
+++ cmd/vk2tg/migrations/0002_vk_post_updates.sql
@@ -1,36 +1,6 @@
 -- +goose Up
-DO $$
-BEGIN
-	IF to_regclass('vk_post') IS NULL THEN
-		IF to_regclass('posted_mesage') IS NOT NULL THEN
-			EXECUTE 'ALTER TABLE posted_mesage RENAME TO vk_post';
-		ELSIF to_regclass('published_posts') IS NOT NULL THEN
-			EXECUTE 'ALTER TABLE published_posts RENAME TO vk_post';
-		ELSE
-			RAISE NOTICE 'table posted_mesage / published_posts not found, creating vk_post from scratch';
-			EXECUTE '
-				CREATE TABLE vk_post (
-					owner_id     BIGINT       NOT NULL,
-					id           BIGINT       NOT NULL,
-					published_at TIMESTAMPTZ,
-					hash         TEXT         NOT NULL DEFAULT '''',
-					PRIMARY KEY (owner_id, id)
-				);
-			';
-		END IF;
-	END IF;
-END $$;
-
-DO $$
-BEGIN
-	IF EXISTS (
-		SELECT 1
-		FROM information_schema.columns
-		WHERE table_name = 'vk_post' AND column_name = 'post_id'
-	) THEN
-		EXECUTE 'ALTER TABLE vk_post RENAME COLUMN post_id TO id';
-	END IF;
-END $$;
+ALTER TABLE posted_mesage RENAME TO vk_post;
+ALTER TABLE vk_post RENAME COLUMN post_id TO id;
 
 ALTER TABLE vk_post
 	ADD COLUMN IF NOT EXISTS hash TEXT NOT NULL DEFAULT '';
